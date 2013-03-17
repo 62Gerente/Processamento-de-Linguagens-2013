7df4c1d931ef6cc683a5f25cd819b27ac81132b7
André Santos <andreccdr@gmail.com>
2013-03-17 00:25:40 +0000

    Adicionadas mais funções LATEX
    Fica a faltar a tabela e o verbatim de linha

--- PAH/functionsLatex.c
+++ PAH/functionsLatex.c
@@ -4,21 +4,30 @@
 #include "functionsLatex.h"
 #include "LinkedList/linkedlist.h"
 
+/* Função que fecha a ultima tag aberta */
 void endLatexTag(LinkedList l){
     printf("%s",(char*)(popLinkedList(l)));
 }
+
+/* Início de negrito em latex */
 void beginBoldLatex(LinkedList l){
     printf("\\textbf{");
     addCloseTagLatex(l);
 }
+
+/* Início de itálico em latex */
 void beginItalicLatex(LinkedList l){
     printf("\\textit{");
     addCloseTagLatex(l);
 }
+
+/* Início de sublinhado em latex */ 
 void beginUnderlineLatex(LinkedList l){
     printf("\\underline{");
     addCloseTagLatex(l);
 }
+
+/* Início de um título em latex */ 
 void beginTitleLatex(LinkedList l,int level){
     int i;
     printf("\\");
@@ -27,9 +36,113 @@ void beginTitleLatex(LinkedList l,int level){
     printf("section{");
     addCloseTagLatex(l);
 }
+
+/* Adiciona á lista ligada a tag default de fim em latex */
 void addCloseTagLatex(LinkedList l){
     pushLinkedList(l,(void*)"}");
 }
+
+/* Tag de índice em latex */
 void addIndexLatex(){
 	printf("\\tableofcontents");
+}
+
+/* Início de uma lista ordenada em latex */
+void beginOrderListLatex(){
+    printf("\\begin{enumerate}");
+}
+
+/* Fim de uma lista ordenada em latex */
+void endOrderListLatex(){
+    printf("\\end{enumerate}");
+}
+
+/* Início de uma lista não ordenada em latex */
+void beginUnorderListLatex(){
+    printf("\\begin{itemize}");
+}
+
+/* Fim de uma lista não ordenada em latex */
+void endUnorderListLatex(){
+    printf("\\end{itemize}");
+}
+
+/* Início de um dicionário em latex */
+void beginDictionaryLatex(){
+    printf("\\begin{description}");
+}
+
+/* Fim de um dicionário em latex */
+void endDictionaryLatex(){
+    printf("\\end{description}");
+}
+
+/* Início de entrada de dicionário */
+void beginDictionaryEntryLatex(LinkedList l){
+    printf("\\item[");
+    pushLinkedList(l,(void*)"]");
+}
+
+/* Adiciona item numa lista */
+void addItemLatex(){
+    printf("\\item ");
+}
+
+/* Início de uma imagem em Latex */
+void beginImageLatex(LinkedList l){
+    printf("\\begin{figure}[h]\n");
+    printf("\\centering\n");
+    printf("\\includegraphics{");
+    pushLinkedList(l,(void*)"}\n\\end{figure}");
+} 
+
+/* Fim do include graphics de uma imagem */
+void endImagePathLatex(){
+    printf("}\n");
+}
+
+/* Início da legenda de uma imagem em Latex */
+void beginImageCaptionLatex(){
+    printf("\\caption{");
+}
+
+/* Início de um link em Latex */
+void beginLinkLatex(LinkedList l){
+    printf("\\href{");
+    addCloseTagLatex(l);
+}
+
+/* Fim do campo URL de um link em Latex */
+void endURLLatex(){
+    printf("}");
+}
+
+/* Início do campo Texto de um link em Latex */
+void beginLinkTextLatex(){
+    printf("{");
+}
+
+/* Comentário de linha em Latex */
+void addLineCommentLatex(){
+    printf("%s","%");
+}
+
+/* Início de comentário multilinha em Latex */
+void beginCommentLatex(){
+    printf("\\begin{comment}");
+}
+
+/* Fim de comentário multilinha em Latex */
+void endCommentLatex(){
+    printf("\\end{comment}");
+}
+
+/* Início de verbatim em Latex */
+void beginVerbatimLatex(){
+    printf("\\begin{verbatim}");
+}
+
+/* Fim de verbatim em Latex */
+void endVerbatimLatex(){
+    printf("\\end{verbatim}");
 }
--- PAH/functionsLatex.h
+++ PAH/functionsLatex.h
@@ -3,12 +3,82 @@
 
 #include "LinkedList/linkedlist.h"
 
+/* Função que fecha a ultima tag aberta */
 void endLatexTag(LinkedList l);
+
+/* Início de negrito em latex */
 void beginBoldLatex(LinkedList l);
+
+/* Início de itálico em latex */
 void beginItalicLatex(LinkedList l);
+
+/* Início de sublinhado em latex */ 
 void beginUnderlineLatex(LinkedList l);
-void beginTitleLatex(LinkedList l, int level);
+
+/* Início de um título em latex */ 
+void beginTitleLatex(LinkedList l,int level);
+
+/* Adiciona á lista ligada a tag default de fim em latex */
 void addCloseTagLatex(LinkedList l);
+
+/* Tag de índice em latex */
 void addIndexLatex();
+
+/* Início de uma lista ordenada em latex */
+void beginOrderListLatex();
+
+/* Fim de uma lista ordenada em latex */
+void endOrderListLatex();
+
+/* Início de uma lista não ordenada em latex */
+void beginUnorderListLatex();
+
+/* Fim de uma lista não ordenada em latex */
+void endUnorderListLatex();
+
+/* Início de um dicionário em latex */
+void beginDictionaryLatex();
+
+/* Fim de um dicionário em latex */
+void endDictionaryLatex();
+
+/* Início de entrada de dicionário */
+void beginDictionaryEntryLatex(LinkedList l);
+
+/* Adiciona item numa lista */
+void addItemLatex();
+
+/* Início de uma imagem em Latex */
+void beginImageLatex(LinkedList l);
+
+/* Fim do include graphics de uma imagem */
+void endImagePathLatex();
+
+/* Início da legenda de uma imagem em Latex */
+void beginImageCaptionLatex();
+
+/* Início de um link em Latex */
+void beginLinkLatex(LinkedList l);
+
+/* Fim do campo URL de um link em Latex */
+void endURLLatex();
+
+/* Início do campo Texto de um link em Latex */
+void beginLinkTextLatex();
+
+/* Comentário de linha em Latex */
+void addLineCommentLatex();
+
+/* Início de comentário multilinha em Latex */
+void beginCommentLatex();
+
+/* Fim de comentário multilinha em Latex */
+void endCommentLatex();
+
+/* Início de verbatim em Latex */
+void beginVerbatimLatex();
+
+/* Fim de verbatim em Latex */
+void endVerbatimLatex();
 
 #endif
