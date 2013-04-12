ca563d241fc637bb5f56dc5756b1fa97217707e3
Pedro Carneiro <pedrosfdcarneiro@gmail.com>
2013-04-12 20:53:17 +0100

    testes modificados

--- PAH/testar.sh
+++ PAH/testar.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+rm exemplosHtml/*.html
+
 for teste in exemplosHtml/*.pah
 do
 	nome=${teste%.pah}	
 	echo "A processar o ficheiro $teste ..."	
-	./pah --html $teste $nome.html 	
+	./pah --html $teste $nome.PAH.html
+	#./pah --html $teste 
+	#./html < $teste >  $nome.HTML.html
 done
 
 
